7bdbfaa72b4fa6e8cef13eedb466b27a60f03a0a
HyeryeonSong <hyeryeon18@gmail.com>
2022-03-01 00:34:18 +0900

    Login modal

--- main.js
+++ main.js
@@ -9,3 +9,22 @@ document.querySelector('.login__btn').addEventListener('click', function(){
 document.querySelector('.modal__close').addEventListener('click', function(){
   document.querySelector('.modal__bg').classList.remove('modal__show');
 })
+
+
+
+$('form').on('submit', function(e){
+  const emailValue = document.getElementById('email').value;
+  const passwordValue = document.getElementById('password').value;
+  console.log(passwordValue.length);
+  if(emailValue == ''){
+    alert('아이디 입력안함')
+    e.preventDefault();
+  }
+  if(passwordValue == ''){
+    alert('비번입력안함');
+    e.preventDefault();
+  } else if (passwordValue.length < 6){
+    alert('비밀번호는 6자리 이상 입력하세요');
+    e.preventDefault();
+  }
+})
