2a5ba61b5879fce465e552c8bace0f7d5a603974
Hyeryeon Song <hyeryeon18@gmail.com>
2022-03-08 17:59:39 +0900

    JS 추가

--- main.js
+++ main.js
@@ -10,23 +10,26 @@ document.querySelector('.modal__close').addEventListener('click', function(){
   document.querySelector('.modal__bg').classList.remove('modal__show');
 })
 
+const form = document.querySelector('form');
+form.addEventListener('submit', e =>{
+  let emailValue = document.getElementById('email').value;
+  let passwordValue = document.getElementById('password').value;
+  let regEmail = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+  let regPassword = /[A-Z]/;
 
-$('form').on('submit', function(e){
-  const emailValue = document.getElementById('email').value;
-  const passwordValue = document.getElementById('password').value;
-  console.log(passwordValue.length);
-  if(emailValue == ''){
-    alert('아이디를 입력해주세요.')
-    e.preventDefault(); // 폼전송막기
+  if(emailValue == '' || !regEmail.test(emailValue)){
+    alert('이메일형식이 아닙니다.')
+    e.preventDefault();// 폼전송막기
   }
-  if(passwordValue == ''){
-    alert('비밀번호를 입력해주세요.');
+  if(passwordValue == '' || !regPassword.test(passwordValue)){
+    console.log
+    alert('비밀번호는 6자리이상, 적어도 대문자 하나가 들어가야 합니다.');
     e.preventDefault();
   } else if (passwordValue.length < 6){
     alert('비밀번호는 6자리 이상 입력하세요');
     e.preventDefault();
-  }
-})
+  }  
+});
 
 // 다크모드 토글
 var count = 0;
@@ -42,7 +45,47 @@ darkmodeBadge.addEventListener('click', function(){
   }
 });
 
+setTimeout(function(){ // 몇초 후 특정 코드 실행(콜백함수, 시간)
+  const alertBox = document.querySelector('.alert');
+  alertBox.style.display = "none";
+}, 5500) 
 
+setInterval(function(){  // 1초마다 시간이 줄어들게
+  let time = document.querySelector('.time');
+  if (time.innerText <= 5){               
+    time.innerText = time.innerText -1;
+    time.innerHTML = time.innerText;    
+  } 
+}, 1000)
+
+// 이미지 슬라이드
+const slideContainer = document.querySelector('.slide__container');
+const slideButton1 = document.querySelector('.slide1');
+const slideButton2 = document.querySelector('.slide2');
+const slideButton3 = document.querySelector('.slide3');
+
+// const nextButton = document.querySelector('.next');
+
+// nextButton.addEventListener('click', nextSlide);
+// var current = 1;
+// function nextSlide(){    
+//   if(current <= 2){
+//     slideContainer.style.transform = 'translateX(-' + current + '00vw)';
+//     console.log(current)  
+//     current += 1;
+//     console.log(current)  
+//   } 
+// }
+
+slideButton1.addEventListener('click', function(){
+  slideContainer.style.transform = 'translateX(0vw)';
+});
+slideButton2.addEventListener('click', function(){
+  slideContainer.style.transform = 'translateX(-100vw)';
+});
+slideButton3.addEventListener('click', function(){
+  slideContainer.style.transform = 'translateX(-200vw)';
+});
 
 
 
